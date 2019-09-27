a3d46a60229bc279eb9464a844f8d89ba262f603
Angie Ta <angielisata@gmail.com>
2019-09-26 23:51:24 -0700

    Updates

---
+++ precache-manifest.3107df96b4ecbfebb83a2bbc8a7f17f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1741077c109d9d0cae93d8af79f8e7dd",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "9ee0dd91e6e9275e6bb6",
+    "url": "/react-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5fd16cb695c089f21055",
+    "url": "/react-test/static/js/2.a1f77696.chunk.js"
+  },
+  {
+    "revision": "9ee0dd91e6e9275e6bb6",
+    "url": "/react-test/static/js/main.41e03e79.chunk.js"
+  },
+  {
+    "revision": "685540923d5180e5e611",
+    "url": "/react-test/static/js/runtime-main.bc7bf75b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-test/static/media/logo.25bf045c.svg"
+  }
+]);
