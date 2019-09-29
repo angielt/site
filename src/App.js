eab0e5739a528e90c0c0e691ee7553ee45f9bd15
Angie Ta <angielisata@gmail.com>
2019-09-29 02:06:20 -0700

    redesign

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// potenial change, click to change language of website/ hover to translate
+
+function Content() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="Content">
+      <div className="card">
+        <div className="card-body">
+        <h4 className="card-title">Who am I?</h4>
+        <p className="card-text">
+        My name is Angie Ta, and I am a web/iOS developer, and aspiring polyglot.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+      </div>
+      <div className="card">
+        <h4 className="card-title">Card title</h4>
+        <div className="card-body">
+        <p className="card-text">Some example text. Some example text.</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Content;
