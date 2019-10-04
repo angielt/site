e2e30d37f016a9e45da2e580842abfab84fa492a
Angie Ta <angielisata@gmail.com>
2019-10-03 20:41:30 -0700

    Updates

---
+++ precache-manifest.e60bf6a8b002c14c67651ef0546c37cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d1999091b3e94b3b9416ca91cf1c35",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "6b086377b1647e518834",
+    "url": "/site/static/css/main.6df43e36.chunk.css"
+  },
+  {
+    "revision": "9b09d4e217f8d133633c",
+    "url": "/site/static/js/2.e0473902.chunk.js"
+  },
+  {
+    "revision": "6b086377b1647e518834",
+    "url": "/site/static/js/main.2346d3ec.chunk.js"
+  },
+  {
+    "revision": "e8a5f1617e36707fced0",
+    "url": "/site/static/js/runtime-main.ed326351.js"
+  },
+  {
+    "revision": "0a043ee5bff0dc9383c23f5d847dc978",
+    "url": "/site/static/media/geordanna-cordero-fields-5NE6mX0WVfQ-unsplash.0a043ee5.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/site/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4618201817553ae08feef53c8eda6ef5.js"
+  "/site/precache-manifest.e60bf6a8b002c14c67651ef0546c37cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
