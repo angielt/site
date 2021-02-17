5640d587ff59ddf40c816655b8269aa3f96f57d3
Angie Ta <angielisata@gmail.com>
2021-02-16 20:30:43 -0800

    Updates

---
+++ precache-manifest.96e210623e076eacfd86fbdbbca224cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb564d9d2f512aa511b15f00d18cc43",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "966cefcb8c60e3d3252b",
+    "url": "/site/static/css/main.798fbed6.chunk.css"
+  },
+  {
+    "revision": "d1ee87f94311cfd62f67",
+    "url": "/site/static/js/2.d65eaae6.chunk.js"
+  },
+  {
+    "revision": "966cefcb8c60e3d3252b",
+    "url": "/site/static/js/main.a48cfc3a.chunk.js"
+  },
+  {
+    "revision": "e8a5f1617e36707fced0",
+    "url": "/site/static/js/runtime-main.ed326351.js"
+  },
+  {
+    "revision": "29b5bd882285779e29411811db4fe27a",
+    "url": "/site/static/media/hello.29b5bd88.svg"
+  },
+  {
+    "revision": "2c85a51bc9bbadc219263a6a983cf020",
+    "url": "/site/static/media/hellofill.2c85a51b.svg"
+  }
+]);
